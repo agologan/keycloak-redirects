89bd6af72e1ab6eb4d6270cce0752a5354d7d475
Alexandru Gologan <alexandru.gologan@gmail.com>
2025-06-23 10:06:20 +0300

    make kube watch more resilient

--- src/services/kubernetes.py
+++ src/services/kubernetes.py
@@ -1,6 +1,8 @@
 from .events import EventProcessor, EventType, KeycloakRealmClient
 from kubernetes import client, config, watch
+from kubernetes.client.exceptions import ApiException
 from utils.unwrap import unwrap
+import urllib3
 
 
 class KubernetesService:
@@ -18,11 +20,25 @@ class KubernetesService:
 
         resource_version = unwrap(ingress_list.metadata).resource_version
 
-        w = watch.Watch()
-        for event in w.stream(
-            self.api.list_ingress_for_all_namespaces, resource_version=resource_version
-        ):
-            modified = self.service.process_k8s_event(
-                event["object"], EventType(event["type"])
-            )
-            callback(self.service.redirects_for_clients(modified))
+        while True:
+            try:
+                w = watch.Watch()
+                for event in w.stream(
+                    self.api.list_ingress_for_all_namespaces,
+                    resource_version=resource_version,
+                ):
+                    modified = self.service.process_k8s_event(
+                        event["object"], EventType(event["type"])
+                    )
+                    callback(self.service.redirects_for_clients(modified))
+            except ApiException as err:
+                if err.status == 410:
+                    print(
+                        "ERROR: The requested resource version is no longer available."
+                    )
+                    resource_version = None
+                else:
+                    raise
+
+            except urllib3.exceptions.ProtocolError:
+                print("Lost connection to the k8s API server. Reconnecting...")
